9083406e6f9a1158ba2fd7a4841e3a2fd0a84dc5
casaxmore <casaxmore@gmail.com>
2023-02-22 12:36:52 +0100

    listo

---
+++ src/app/shared/directives/err-msg.directive.ts
@@ -0,0 +1,75 @@
+import { Directive, ElementRef, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+
+@Directive({
+  selector: '[error-msg]'
+})
+export class ErrMsgDirective implements OnInit, OnChanges {
+
+  private _color: string = 'red';
+  private _mensaje: string = 'Este campo es requerido';
+
+  htmlElement: ElementRef<HTMLElement>;
+  /* @Input() color: string = 'red'; */
+  @Input() set color(valor: string){
+    this._color = valor;
+    this.setColor();
+      /* this.htmlElement.nativeElement.style.color = valor; */
+  }
+
+  /* @Input() mensaje: string = 'Este campo es necesario'; */
+  @Input() set mensaje(valor: string) {
+    this._mensaje = valor;
+    this.setMensaje();
+    /* this.htmlElement.nativeElement.innerText = valor; */
+  }
+
+  @Input() set valido(valor: boolean) {
+    if(valor === true) {
+      this.htmlElement.nativeElement.classList.add('hidden');
+    }else{
+      this.htmlElement.nativeElement.classList.remove('hidden');
+    }
+  }
+
+  constructor( private el: ElementRef<HTMLElement>) {
+    this.htmlElement = el;
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+
+    /* if(changes['mensaje']) {
+      const mensaje = changes['mensaje'].currentValue;
+      this.htmlElement.nativeElement.innerText = mensaje;
+    }
+
+    if(changes['color']) {
+      const color = changes['color'].currentValue;
+      this.htmlElement.nativeElement.style.color = color;
+    }
+
+    console.log(changes); */
+  }
+
+  ngOnInit(): void {
+/*
+    console.log(this.color); // undefined
+    console.log(this.mensaje); // undefined
+ */
+    this.setColor();
+    this.setMensaje();
+    this.setEstilo();
+  }
+
+  setEstilo(): void {
+    this.htmlElement.nativeElement.classList.add('form-text');
+  }
+
+  setColor(): void {
+    this.htmlElement.nativeElement.style.color = this._color;
+  }
+
+  setMensaje(): void {
+    this.htmlElement.nativeElement.innerText = this._mensaje;
+  }
+
+
+}
